09a03cfd3946a59135996641c3d098b142d01f51
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-07-06 12:34:22 +0530

    Change to JWT

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,6 +2,10 @@ import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
 export default NextAuth({
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: 'jwt',
+  },
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
