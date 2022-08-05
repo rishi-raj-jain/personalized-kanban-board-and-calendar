33cb7b492bd91247f8d9222de89624a503b21e5e
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2022-08-05 10:28:53 +0000

    Dictate tasks

--- pages/bot.js
+++ pages/bot.js
@@ -24,7 +24,7 @@ const Dictaphone = () => {
       callback: () => router.push('/calendar'),
     },
     {
-      command: ['Tell me the number of tasks due today'],
+      command: ['Tell all my task due today', 'Tell all my tasks due today', 'Tell me the number of task due today', 'Tell me the number of tasks due today'],
       callback: () => {
         if (status === 'authenticated') {
           fetch(`/api/acitivity?userEmail=${userData.user.email}`)
@@ -33,6 +33,11 @@ const Dictaphone = () => {
               if (res.hasOwnProperty('Cards')) {
                 speak({ text: `You have ${res['Cards'].length} tasks due today.` })
                 setMessage('Total Number of Tasks are:', res['Cards'].length)
+                speak({ text: `Let's go over each of them.` })
+                res['Cards'].forEach((i, _ind) => {
+                  let date = new Date(`${i.date} ${i.time}`)
+                  speak({ text: `Task ${_ind + 1}: ${i.title || 'No Title'} due on ${date.getDate()} ${month[date.getMonth()]}, ${date.getFullYear()}` })
+                })
               } else {
                 setMessage('Failed to load all your tasks.')
               }
@@ -43,30 +48,6 @@ const Dictaphone = () => {
         } else {
           console.log(e)
           setMessage('You need to log in to view tasks.')
-        }
-      },
-    },
-    {
-      command: 'Tell all my tasks due today',
-      callback: () => {
-        if (status === 'authenticated') {
-          fetch(`/api/acitivity?userEmail=${userData.user.email}`)
-            .then((res) => res.json())
-            .then((res) => {
-              if (res.hasOwnProperty('Cards')) {
-                res['Cards'].forEach((i, _ind) => {
-                  let date = new Date(`${i.date} ${i.time}`)
-                  speak({ text: `Task ${_ind+1}: ${i.title || 'No Title'} due on ${date.getDate()} ${month[date.getMonth()]}, ${date.getFullYear()}` })
-                })
-                setMessage('Total Number of Tasks are:', res['Cards'].length)
-              } else {
-                setMessage('Failed to load all your tasks.')
-              }
-            })
-            .catch((e) => {
-              console.log(e)
-              setMessage('Failed to load all your tasks.')
-            })
         }
       },
     },
