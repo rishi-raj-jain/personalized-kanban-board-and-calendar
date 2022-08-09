77c9fdefbb6c71ccf337fb3e13be1c243909860c
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-08-10 02:28:18 +0530

    Subtract one month

--- components/Day.js
+++ components/Day.js
@@ -7,15 +7,15 @@ export default function Day({ day, rowIdx, filteredEvents }) {
   const [dayEvents, setDayEvents] = useState([])
 
   useEffect(() => {
-    const events = filteredEvents.filter((evt) => dayjs(evt.date).format('DD-MM-YY') === day.format('DD-MM-YY'))
+    const events = filteredEvents.filter((evt) => dayjs(evt.date).subtract(1, 'month').format('DD-MM-YY') === day.format('DD-MM-YY'))
     setDayEvents(events)
   }, [filteredEvents, day])
 
   function getCurrentDayClass() {
-    return day.format('DD-MM-YY') === dayjs().format('DD-MM-YY') ? 'bg-cyan-600 px-2 py-1 text-white rounded-full' : ''
+    return day.format('DD-MM-YY') === dayjs().subtract(1, 'month').format('DD-MM-YY') ? 'bg-cyan-600 px-2 py-1 text-white rounded-full' : ''
   }
   return (
-    <div className="flex flex-col items-center border border-[#a0a0a040] shadow-lg overflow-y-scroll">
+    <div className="flex flex-col items-center overflow-y-scroll border border-[#a0a0a040] shadow-lg">
       <header className="flex flex-col items-center gap-y-2">
         <span className="mt-3 text-sm font-bold text-white">{day.format('ddd')}</span>
         <span className={`text-center text-sm text-gray-200 ${getCurrentDayClass()}`}>
